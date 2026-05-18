1ddc0dc0414e542ad08cfbdcfea38938be84347d
JunkyDeveloper <junkydeveloper@gmail.com>
2026-05-18 02:30:13 +0200

    rustrover support added

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,17 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.2.6.2")
+        rustRover(providers.gradleProperty("platformVersion"))
+        bundledPlugin("com.jetbrains.rust")
         testFramework(TestFrameworkType.Platform)
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "261"
+            untilBuild = provider { null }
+        }
     }
 }
